3f4af7638bd7a468778b87f49e8c2e4891ba34a0
kyrie0126 <kylewong00@gmail.com>
2022-11-13 12:57:31 -0500

    added error checks

--- main.py
+++ main.py
@@ -4,9 +4,25 @@ import pandas as pd
 # Create a dictionary of letters and phonetic code words
 nato_dict = {row.letter: row.code for (index, row) in pd.read_csv("nato_phonetic_alphabet.csv").iterrows()}
 
+
 # Create a list of the phonetic code words from a word that the user inputs
-name = input("What's your name? ").upper()
-nato_name = [nato_dict[letter] for letter in name if letter in nato_dict]
-print(f"Your NATO phonetic name is: {nato_name}")
+# Check for exceptions/errors that would crash program
+def name_convert():
+    name = input("What's your name? ").upper()
+    try:
+        nato_name = [nato_dict[letter] for letter in name]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+    else:
+        print(f"Your NATO phonetic name is: {nato_name}")
+    finally:
+        name_convert()
+
+
+name_convert()
+
+
+
+
 
 
